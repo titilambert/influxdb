f2b78bad5230bbf07b59522df6c9d22d14e90cba
Ross McDonald <ross@influxdb.com>
2015-11-24 17:53:48 -0600

    Re-enabling support for ARM builds in `build.py`. Improved `build.py`'s iteration and package version. Fixed minor bug with logrotate file in `build.py`. Improved installation pre and post install/uninstall scripts by moving to bash and adding distribution-specific logic to account for quirks between the different package managers.

--- build.py
+++ build.py
@@ -3,7 +3,6 @@
 # This is the InfluxDB build script.
 #
 # Current caveats:
-#   - Does not currently build ARM builds/packages
 #   - Does not checkout the correct commit/branch (for now, you will need to do so manually)
 #   - Has external dependencies for packaging (fpm) and uploading (boto)
 #
@@ -82,13 +81,12 @@ targets = {
 }
 
 supported_builds = {
-    # TODO(rossmcdonald): Add support for multiple GOARM values
     'darwin': [ "amd64", "386" ],
     # Windows is not currently supported in InfluxDB 0.9.5 due to use of mmap
     # 'windows': [ "amd64", "386", "arm" ],
     'linux': [ "amd64", "386", "arm" ]
 }
-supported_go = [ '1.5.1', '1.4.2' ]
+
 supported_packages = {
     "darwin": [ "tar", "zip" ],
     "linux": [ "deb", "rpm", "tar", "zip" ],
@@ -104,17 +102,19 @@ def run(command, allow_failure=False, shell=False):
             out = subprocess.check_output(command.split(), stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         print ""
+        print ""
         print "Executed command failed!"
         print "-- Command run was: {}".format(command)
         print "-- Failure was: {}".format(e.output)
         if allow_failure:
             print "Continuing..."
-            return out
+            return None
         else:
             print ""
             print "Stopping."
             sys.exit(1)
     except OSError as e:
+        print ""
         print ""
         print "Invalid command!"
         print "-- Command run was: {}".format(command)
@@ -152,7 +152,10 @@ def get_current_branch():
     return out.strip()
 
 def get_system_arch():
-    return os.uname()[4]
+    arch = os.uname()[4]
+    if arch == "x86_64":
+        arch = "amd64"
+    return arch
 
 def get_system_platform():
     if sys.platform.startswith("linux"):
@@ -179,12 +182,12 @@ def check_path_for(b):
 
 def check_environ(build_dir = None):
     print "\nChecking environment:"
-    for v in [ "GOPATH", "GOBIN" ]:
+    for v in [ "GOPATH", "GOBIN", "GOROOT" ]:
         print "\t- {} -> {}".format(v, os.environ.get(v))
 
     cwd = os.getcwd()
     if build_dir == None and os.environ.get("GOPATH") and os.environ.get("GOPATH") not in cwd:
-        print "\n!! WARNING: Your current directory is not under your GOPATH! This probably won't work."
+        print "\n!! WARNING: Your current directory is not under your GOPATH. This may lead to build failures."
 
 def check_prereqs():
     print "\nChecking for dependencies:"
@@ -282,7 +285,8 @@ def build(version=None,
           rc=None,
           race=False,
           clean=False,
-          outdir="."):
+          outdir=".",
+          goarm_version="6"):
     print "-------------------------"
     print ""
     print "Build plan:"
@@ -293,6 +297,8 @@ def build(version=None,
     print "\t- branch: {}".format(branch)
     print "\t- platform: {}".format(platform)
     print "\t- arch: {}".format(arch)
+    if arch == 'arm' and goarm_version:
+        print "\t- ARM version: {}".format(goarm_version)
     print "\t- nightly? {}".format(str(nightly).lower())
     print "\t- race enabled? {}".format(str(race).lower())
     print ""
@@ -312,10 +318,11 @@ def build(version=None,
     for b, c in targets.iteritems():
         print "\t- Building '{}'...".format(os.path.join(outdir, b)),
         build_command = ""
-        build_command += "GOOS={} GOOARCH={} ".format(platform, arch)
-        if arch == "arm":
-            # TODO(rossmcdonald): Add GOARM variables for ARM builds
-            build_command += "GOOARM={} ".format(6)
+        build_command += "GOOS={} GOARCH={} ".format(platform, arch)
+        if arch == "arm" and goarm_version:
+            if goarm_version not in ["5", "6", "7", "arm64"]:
+                print "!! Invalid ARM build version: {}".format(goarm_version)
+            build_command += "GOARM={} ".format(goarm_version)
         build_command += "go build -o {} ".format(os.path.join(outdir, b))
         if race:
             build_command += "-race "
@@ -331,7 +338,7 @@ def build(version=None,
             build_command += "-X main.branch={} ".format(branch)
             build_command += "-X main.commit={}\" ".format(get_current_commit())
         build_command += c
-        out = run(build_command, shell=True)
+        run(build_command, shell=True)
         print "[ DONE ]"
     print ""
 
@@ -373,8 +380,8 @@ def package_scripts(build_root):
     os.chmod(os.path.join(build_root, SCRIPT_DIR[1:], INIT_SCRIPT.split('/')[1]), 0644)
     shutil.copyfile(SYSTEMD_SCRIPT, os.path.join(build_root, SCRIPT_DIR[1:], SYSTEMD_SCRIPT.split('/')[1]))
     os.chmod(os.path.join(build_root, SCRIPT_DIR[1:], SYSTEMD_SCRIPT.split('/')[1]), 0644)
-    shutil.copyfile(LOGROTATE_SCRIPT, os.path.join(build_root, SCRIPT_DIR[1:], LOGROTATE_SCRIPT.split('/')[1]))
-    os.chmod(os.path.join(build_root, SCRIPT_DIR[1:], LOGROTATE_SCRIPT.split('/')[1]), 0644)
+    shutil.copyfile(LOGROTATE_SCRIPT, os.path.join(build_root, LOGROTATE_DIR[1:], "influxdb"))
+    os.chmod(os.path.join(build_root, LOGROTATE_DIR[1:], "influxdb"), 0644)
     shutil.copyfile(DEFAULT_CONFIG, os.path.join(build_root, CONFIG_DIR[1:], "influxdb.conf"))
     os.chmod(os.path.join(build_root, CONFIG_DIR[1:], "influxdb.conf"), 0644)
 
@@ -426,6 +433,8 @@ def build_packages(build_output, version, nightly=False, rc=None, iteration=1):
                 for package_type in supported_packages[p]:
                     print "\t- Packaging directory '{}' as '{}'...".format(build_root, package_type),
                     name = "influxdb"
+                    package_version = version
+                    package_iteration = iteration
                     if package_type in ['zip', 'tar']:
                         if nightly:
                             name = '{}-nightly_{}_{}'.format(name, p, a)
@@ -434,25 +443,19 @@ def build_packages(build_output, version, nightly=False, rc=None, iteration=1):
                     if package_type == 'tar':
                         # Add `tar.gz` to path to ensure a small package size
                         current_location = os.path.join(current_location, name + '.tar.gz')
-                    if package_type == 'deb' and rc:
-                        # For debs with an RC, just append to version number
-                        version += "-rc{}".format(rc)
-                    fpm_command = "fpm {} --name {} -a {} -t {} --version {} -C {} -p {} ".format(
+                    if rc is not None:
+                        package_iteration = "0.rc{}".format(rc)
+                    fpm_command = "fpm {} --name {} -a {} -t {} --version {} --iteration {} -C {} -p {} ".format(
                         fpm_common_args,
                         name,
                         a,
                         package_type,
-                        version,
+                        package_version,
+                        package_iteration,
                         build_root,
                         current_location)
                     if package_type == "rpm":
                         fpm_command += "--depends coreutils "
-                        # For rpms with RC, add to iteration for adherence to Fedora packaging standard:
-                        # http://fedoraproject.org/wiki/Packaging%3aNamingGuidelines#NonNumericRelease
-                        if rc:
-                            fpm_command += "--iteration 0.{}.rc{} ".format(iteration, rc)
-                        else:
-                            fpm_command += "--iteration 1 ".format(iteration)
                     out = run(fpm_command, shell=True)
                     matches = re.search(':path=>"(.*)"', out)
                     outfile = None
@@ -462,9 +465,7 @@ def build_packages(build_output, version, nightly=False, rc=None, iteration=1):
                         print "[ COULD NOT DETERMINE OUTPUT ]"
                     else:
                         # Strip nightly version (the unix epoch) from filename
-                        if nightly and package_type == 'deb':
-                            outfile = rename_file(outfile, outfile.replace("{}".format(version), "nightly"))
-                        elif nightly and package_type == 'rpm':
+                        if nightly and package_type in ['deb', 'rpm']:
                             outfile = rename_file(outfile, outfile.replace("{}-1".format(version), "nightly"))
                         outfiles.append(os.path.join(os.getcwd(), outfile))
                         print "[ DONE ]"
@@ -482,18 +483,20 @@ def print_usage():
     print "Options:"
     print "\t --outdir=<path> \n\t\t- Send build output to a specified path. Defaults to ./build."
     print "\t --arch=<arch> \n\t\t- Build for specified architecture. Acceptable values: x86_64|amd64, 386, arm, or all"
+    print "\t --goarm=<arm version> \n\t\t- Build for specified ARM version (when building for ARM). Default value is: 6"
     print "\t --platform=<platform> \n\t\t- Build for specified platform. Acceptable values: linux, windows, darwin, or all"
     print "\t --version=<version> \n\t\t- Version information to apply to build metadata. If not specified, will be pulled from repo tag."
     print "\t --commit=<commit> \n\t\t- Use specific commit for build (currently a NOOP)."
     print "\t --branch=<branch> \n\t\t- Build from a specific branch (currently a NOOP)."
     print "\t --rc=<rc number> \n\t\t- Whether or not the build is a release candidate (affects version information)."
+    print "\t --iteration=<iteration number> \n\t\t- The iteration to display on the package output (defaults to 0 for RC's, and 1 otherwise)."    
     print "\t --race \n\t\t- Whether the produced build should have race detection enabled."
     print "\t --package \n\t\t- Whether the produced builds should be packaged for the target platform(s)."
     print "\t --nightly \n\t\t- Whether the produced build is a nightly (affects version information)."
     print "\t --update \n\t\t- Whether dependencies should be updated prior to building."
     print "\t --test \n\t\t- Run Go tests. Will not produce a build."
     print "\t --parallel \n\t\t- Run Go tests in parallel up to the count specified."
-    print "\t --timeout \n\t\t- Timeout for Go tests. Default 480s"
+    print "\t --timeout \n\t\t- Timeout for Go tests. Defaults to 480s."
     print "\t --clean \n\t\t- Clean the build output directory prior to creating build."
     print ""
 
@@ -520,6 +523,7 @@ def main():
     timeout = None
     iteration = 1
     no_vet = False
+    goarm_version = "6"
 
     for arg in sys.argv[1:]:
         if '--outdir' in arg:
@@ -574,6 +578,9 @@ def main():
             iteration = arg.split("=")[1]
         elif '--no-vet' in arg:
             no_vet = True
+        elif '--goarm' in arg:
+            # Signifies GOARM flag to pass to build command when compiling for ARM
+            goarm_version = arg.split("=")[1]
         elif '--help' in arg:
             print_usage()
             return 0
@@ -599,12 +606,16 @@ def main():
     if not branch:
         branch = get_current_branch()
     if not target_arch:
-        target_arch = get_system_arch()
+        if 'arm' in get_system_arch():
+            # Prevent uname from reporting ARM arch (eg 'armv7l')
+            target_arch = "arm"
+        else:
+            target_arch = get_system_arch()
     if not target_platform:
         target_platform = get_system_platform()
-
-    if target_arch == "x86_64":
-        target_arch = "amd64"
+    if rc:
+        # If a release candidate, set iteration to 0 (instead of 1)
+        iteration = 0
 
     build_output = {}
     # TODO(rossmcdonald): Prepare git repo for build (checking out correct branch/commit, etc.)
@@ -645,7 +656,8 @@ def main():
                   rc=rc,
                   race=race,
                   clean=clean,
-                  outdir=od)
+                  outdir=od,
+                  goarm_version=goarm_version)
             build_output.get(platform).update( { arch : od } )
 
     # Build packages
--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-# Copy existing configuration if pre-existing installation is found
-test -f /etc/opt/influxdb/influxdb.conf
-if [ $? -eq 0 ]; then
-    mkdir /etc/influxdb
-    cp -a /etc/opt/influxdb/* /etc/influxdb/
+if [[ -f /etc/opt/influxdb/influxdb.conf ]]; then
+    # Legacy configuration found
+    
+    # Create new configuration location (if not already there)
+    test -d /etc/influxdb || mkdir -p /etc/influxdb
+    
+    if [[ ! -f /etc/influxdb/influxdb.conf ]]; then
+	# New configuration does not exist, move legacy configuration to new location
+	cp --backup --suffix=.$(date +%s).install-backup -a /etc/opt/influxdb/* /etc/influxdb/
+    fi
 fi
-
-exit
